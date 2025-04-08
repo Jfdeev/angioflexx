caf44930fa83f4d96bf8addc0761ec15137e1ac9
Jfdeev <jfrsoares161205@gmail.com>
2025-04-08 09:21:42 -0300

    feat: speed insights vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
 import Main from './screens/Main';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+
+injectSpeedInsights();
 
 function App() {
   return (
